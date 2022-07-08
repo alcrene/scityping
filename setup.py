4590850f041e7c939b2dac0a58ee107e63a70295
Alexandre René <alex.c.rene@gmail.com>
2022-07-08 17:06:15 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where="scityping"),  # Required
+    packages=["scityping"],  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
